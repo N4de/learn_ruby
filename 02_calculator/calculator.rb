c2e31ef601673302f0c956524df01fd8ab737c24
N4de <JaAnnala@Gmail.com>
2016-09-19 11:10:16 +0300

    pass tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add num1, num2
+	return num1 + num2
+end
+
+def subtract num1, num2
+	return num1 - num2
+end
+
+def sum list
+	total = 0
+	list.each do |i|
+		total += i
+	end
+	return total
+end
